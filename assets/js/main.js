24a83258c5cc9bd1fc532d9c54afcd52aa3fe53d
massndongo86@gmail.com <massndongo86@gmail.com>
2023-09-11 17:10:44 +0000

    Mode Sombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,17 @@
   // Sélectionnez tous les liens de la navbar avec la classe "nav-link"
-  var navLinks = document.querySelectorAll('.nav-link');
-  var downloadButton = document.getElementById('downloadButton');
+
   // Parcourez tous les liens et ajoutez un gestionnaire d'événement pour le clic
-  navLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      event.preventDefault(); // Empêche le comportement par défaut du navigateur
-      var target = this.getAttribute('href'); // Récupère l'attribut href du lien
-      console.log(target);
-      scrollToSection(target); // Appelle la fonction de défilement vers la section cible
-      // document.querySelector('#propos').style.marginTop = '30%';
-      navLinks.forEach(function(link) {
-        link.classList.remove('active');
-      });
-      link.classList.add('active');
-      });
 
-  });
-
-  // Fonction pour déplacer l'utilisateur vers une section spécifique
-  function scrollToSection(sectionId) {
-    var section = document.querySelector(sectionId);
-  
-    if (section) {
-      section.scrollIntoView({
-        behavior: 'smooth'
-      });
-  
-      var marginTop = 0.3; // Ajustez la valeur de la marge supérieure en pourcentage
-      var sectionTop = section.getBoundingClientRect().top + window.scrollY;
-      var offset = sectionTop - (window.innerHeight * marginTop);
-      
-  
-      window.scrollTo({
-        top: offset,
-        behavior: 'smooth'
-      });
-    }
-  }
-
-  
-  var backToTopBtn = document.getElementById("back-to-top");
-
-  // Afficher ou masquer le bouton en fonction du défilement de la page
-  window.addEventListener("scroll", function() {
-    if (window.scrollY > 100) {
-      backToTopBtn.style.display = "block";
-    } else {
-      backToTopBtn.style.display = "none";
-    }
-  });
-
-  // Faire défiler vers le haut de la page lorsque le bouton est cliqué
-  backToTopBtn.addEventListener("click", function() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
-
-  function downloadFile(url) {
-    var link = document.createElement('a');
-    link.href = url;
-    link.target = '_blank'; // Ouvre le lien dans un nouvel onglet
-    link.download = 'CVMassam.pdf'; // Nom du fichier à télécharger
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
 
   document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('.nav-link');
+    var downloadButton = document.getElementById('downloadButton');
+    var navbar = document.querySelector('.navbar');
+    var btnCv = document.querySelector('.btnCv');
+    var btnMode = document.querySelector('.btnMode');
+    const imageModeClair = document.getElementById('logoClair');
+    const imageModeSombre = document.getElementById('logoSombre');
     const startchange = document.querySelector('#startchange');
-    const navbar = document.querySelector('.navbar');
     const offset = startchange.getBoundingClientRect().top;
   
     document.addEventListener('scroll', function() {
@@ -82,11 +23,124 @@
         navbar.style.backgroundColor = 'transparent';
       }
     });
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du navigateur
+        var target = this.getAttribute('href'); // Récupère l'attribut href du lien
+        console.log(target);
+        scrollToSection(target); // Appelle la fonction de défilement vers la section cible
+        // document.querySelector('#propos').style.marginTop = '30%';
+        navLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+        link.classList.add('active');
+        });
+  
+    });
+  
+    // Fonction pour déplacer l'utilisateur vers une section spécifique
+    function scrollToSection(sectionId) {
+      var section = document.querySelector(sectionId);
+    
+      if (section) {
+        section.scrollIntoView({
+          behavior: 'smooth'
+        });
+    
+        var marginTop = 0.3; // Ajustez la valeur de la marge supérieure en pourcentage
+        var sectionTop = section.getBoundingClientRect().top + window.scrollY;
+        var offset = sectionTop - (window.innerHeight * marginTop);
+        
+    
+        window.scrollTo({
+          top: offset,
+          behavior: 'smooth'
+        });
+      }
+    }
+  
+    
+    var backToTopBtn = document.getElementById("back-to-top");
+  
+    // Afficher ou masquer le bouton en fonction du défilement de la page
+    window.addEventListener("scroll", function() {
+      if (window.scrollY > 100) {
+        backToTopBtn.style.display = "block";
+      } else {
+        backToTopBtn.style.display = "none";
+      }
+    });
+  
+    // Faire défiler vers le haut de la page lorsque le bouton est cliqué
+    backToTopBtn.addEventListener("click", function() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  
+    function downloadFile(url) {
+      var link = document.createElement('a');
+      link.href = url;
+      link.target = '_blank'; // Ouvre le lien dans un nouvel onglet
+      link.download = 'CVMassam.pdf'; // Nom du fichier à télécharger
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
 
     downloadButton.addEventListener('click', function() {
       var cvUrl = 'cv/CVMassam.pdf'; // Remplacez par le chemin réel de votre CV
       downloadFile(cvUrl);
     });
+
+    const toggleButton = document.getElementById('toggleButton');
+    const body = document.body;
+
+    imageModeSombre.style.display = 'none';
+    toggleButton.addEventListener('click', () => {
+      if (body.classList.contains('dark-mode')) {
+          body.classList.remove('dark-mode');
+          body.classList.add('light-mode');
+          navbar.classList.add('bg-body');
+          navbar.classList.remove('bg-dark');
+          btnCv.classList.add('btn-dark');
+          btnCv.classList.remove('btn-light');
+          btnMode.classList.add('btn-outline-dark');
+          btnMode.classList.remove('btn-outline-light');
+          imageModeClair.style.display = 'block';
+          imageModeSombre.style.display = 'none';
+          document.querySelectorAll('.link-icons-rs').forEach(function (bgIcons) {
+            bgIcons.classList.add('link-dark');
+            bgIcons.classList.remove('link-light');
+          });
+          document.querySelectorAll('.nav-link').forEach(function(link) {
+            link.style.color = '';
+          });
+      } else {
+          body.classList.remove('light-mode');
+          body.classList.add('dark-mode');
+          navbar.classList.remove('bg-body');
+          navbar.classList.add('bg-dark');
+          btnCv.classList.remove('btn-dark');
+          btnCv.classList.add('btn-light');
+          btnMode.classList.add('btn-outline-light');
+          btnMode.classList.remove('btn-outline-dark');
+          imageModeClair.style.display = 'none';
+          imageModeSombre.style.display = 'block';
+          document.querySelectorAll('.link-icons-rs').forEach(function (bgIcons) {
+            bgIcons.classList.remove('link-dark');
+            bgIcons.classList.add('link-light');
+          });
+          document.querySelectorAll('.nav-link').forEach(function(link) {
+            link.style.color = 'white';
+          });
+
+      } 
+    });
     
   });
+  
+
+
   
